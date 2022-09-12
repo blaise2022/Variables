d18b2eba1523cf07aa513ef2b72ae994c2a3f18f
blaise <blaisegm31@gmail.com>
2022-09-12 09:42:44 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte blaise = 16;
+        short lisd = 30000;
+        int dallas = 2000000;
+        long world = 800000000;
+        float degrees = 20.998F;
+        double measure = 709654.187543789;
+        char letter = 'A';
+        boolean wateron = true;
+
+
+        System.out.println(blaise);
+        System.out.println(lisd);
+        System.out.println(dallas);
+        System.out.println(world);
+        System.out.println(degrees);
+        System.out.println(measure);
+        System.out.println(letter);
+        System.out.println(wateron);
+    }
+}
